2d0ddfe16b8d573a30c3b8c1d1745ba96230fdc0
leeyujeong <lyj0047@naver.com>
2023-02-01 01:08:11 +0900

    useRef로 focus 주기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -14,8 +17,18 @@ const DiaryEditor = () => {
     });
   };
   const handleSubmit = (e) => {
-    console.log(state);
-    alert("저장성공");
+    if (state.author.length < 1) {
+      // focus
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      // focus
+      contentInput.current.focus();
+
+      return;
+    }
+    alert("저장 성공");
   };
 
   return (
@@ -23,6 +36,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -30,6 +44,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
